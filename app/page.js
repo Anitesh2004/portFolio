0d1e14aa7e920bbb5a7522e9860c6181348c302c
anitesh <anitesh6307@gmail.com>
2025-04-16 01:01:29 +0530

    fixed

--- app/page.js
+++ app/page.js
@@ -1,13 +1,21 @@
 "use client"
 
+
+
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
-import HeroSection from "./components/homepage/hero-section";
+// import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
+
+import dynamic from "next/dynamic";
+
+const HeroSection = dynamic(() => import("./components/homepage/hero-section"), {
+  ssr: false,
+});
 
 
 
